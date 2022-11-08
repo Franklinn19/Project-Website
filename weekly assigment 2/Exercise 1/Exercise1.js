278fd92a709b41cd843ea274c698cd612320089f
Franklinn19 <117764814+Franklinn19@users.noreply.github.com>
2022-11-08 15:42:36 -0500

    Add files via upload

---
+++ weekly assigment 2/Exercise 1/Exercise1.js
@@ -0,0 +1,32 @@
+
+   let number =5;
+
+    let divtable = document.getElementById("table1");
+    divtable.innerText= number + " * " + " 1 " + " = " + number*1;
+
+    let divtable2 = document.getElementById("table2");
+    divtable2.innerText= number + " * " + " 2 " + " = " + number*2;
+   
+    let divtable3 = document.getElementById("table3");
+    divtable3.innerText= number + " * " + " 3 " + " = " + number*3;
+   
+        let divtable4 = document.getElementById("table4");
+    divtable4.innerText= number + " *  " + " 4 " + " = " + number*4;
+    
+    let divtable5 = document.getElementById("table5");
+    divtable5.innerText= number + " * " + " 5 " + " = " + number*5;
+    
+    let divtable6 = document.getElementById("table6");
+    divtable6.innerText= number + "*" + " 6 " + " = " + number*6;
+    
+    let divtable7 = document.getElementById("table7");
+    divtable7.innerText= number + " * " + " 7 " + " = " + number*7;
+    
+    let divtable8 = document.getElementById("table8");
+    divtable8.innerText= number + " * " + " 8 " + " = " + number*8;
+   
+    let divtable9 = document.getElementById("table9");
+    divtable9.innerText= number + " * " + " 9 " + " = " + number*9;
+    
+    let divtable10 = document.getElementById("table10");
+    divtable10.innerText= number + " * " + " 10 " + " = " + number*10;
